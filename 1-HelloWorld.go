9160b7ec1e6151f152e8ebb8e8cb9331621495e7
Ali Emre <33875213+MrBal@users.noreply.github.com>
2021-11-19 00:22:56 +0300

    Create 1-HelloWorld.go

---
+++ 1-HelloWorld.go
@@ -0,0 +1,10 @@
+// The most important thing to do when getting started with any programming language.
+// "Hello World" in GoLang.
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World")
+}
